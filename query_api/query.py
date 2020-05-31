39104c86f9f65a275acfe1557da305ab0f654811
Christian Cabato <ChristianMicar.Cabato@uts.edu.au>
2020-05-31 23:53:28 +1000

    [CC] Naming convention

--- query_api/query.py
+++ query_api/query.py
@@ -6,15 +6,15 @@ def searchUsers():
 
     # Call local API for users
     response = requests.get("http://localhost:8000/query/users/")
-    data = response.json()
+    userData = response.json()
 
     # For debugging purposes
     #print(data)
 
     name = str(input('Enter name: '))
-    for key in data:    
-        if (data[key]['FirstName'] == name or data[key]['LastName'] == name):
-            print(data[key]['FirstName'] + ' ' + data[key]['LastName'])
+    for key in userData:    
+        if (userData[key]['FirstName'] == name or userData[key]['LastName'] == name):
+            print(userData[key]['FirstName'] + ' ' + userData[key]['LastName'])
 
 ################################################################################
 
